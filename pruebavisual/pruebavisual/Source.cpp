f3fb8b5c1f71ab15ca552d3feaeb88ff0d0f93eb
Fernando <55459941+FernadoVL97@users.noreply.github.com>
2019-09-17 14:57:05 -0500

    Update Source.cpp

--- pruebavisual/pruebavisual/Source.cpp
+++ pruebavisual/pruebavisual/Source.cpp
@@ -1,1 +1,10 @@
-#include <iostream>
+#include <iostream> 
+#include <conio.h> 
+using namespace System; 
+using namespace std; 
+ 
+int main(){ 
+cout<<"Hola"   <<endl; 
+  _getch(); 
+  return 0 
+}
